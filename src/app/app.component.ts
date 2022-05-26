61963800d256c07241177422b7422e2c7a37c08e
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-05-26 11:34:07 +0200

    agencias creadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blr-astro-bookings';
+  public title = 'Astro Bookings';
+  public subtitle = 'Welcome on Board';
+  public autor = 'Bruno Lopez';
+  public autorUrl = 'https://www.instagram.com/frostsky_3';
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+  public reloading = false;
+  public getAgenciesLength(){
+    return this.agencies.length;
+  }
+  public reload(list: string){
+    console.log("reloading...");
+    this.reloading= true;
+
+  }
+
 }
