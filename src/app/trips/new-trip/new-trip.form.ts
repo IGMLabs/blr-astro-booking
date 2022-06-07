4926505ab55700c7cb552ad95e1d2555f3d904c0
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-06-07 12:31:07 +0200

    a

--- src/app/trips/new-trip/new-trip.form.ts
+++ src/app/trips/new-trip/new-trip.form.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators, AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  Validators,
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-trip-form',
   templateUrl: './new-trip.form.html',
-  styleUrls: ['./new-trip.form.css']
+  styleUrls: ['./new-trip.form.css'],
 })
 export class NewTripForm implements OnInit {
-
   autoN = 5;
   public form: FormGroup;
   public agencies = [
@@ -24,16 +30,31 @@ export class NewTripForm implements OnInit {
   public statuses = ['Confirmed', 'Waiting'];
 
   constructor(formBuilder: FormBuilder) {
-    this.form = formBuilder.group({
-      destination: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
-      agencyId: new FormControl('', [Validators.required]),
-      plazas: new FormControl('', [Validators.required, Validators.min(1), Validators.max(10)]),
-      precio: new FormControl('', [Validators.required, Validators.min(1000000), Validators.max(10000000)]),
-      dateFrom: new FormControl('', [Validators.required,]),
-      dateTo: new FormControl('', [Validators.required ]),
-    }, {
-      validators: [this.dateControl],
-    });
+    this.form = formBuilder.group(
+      {
+        destination: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(20),
+        ]),
+        agencyId: new FormControl('', [Validators.required]),
+        plazas: new FormControl('', [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(10),
+        ]),
+        precio: new FormControl('', [
+          Validators.required,
+          Validators.min(1000000),
+          Validators.max(10000000),
+        ]),
+        dateFrom: new FormControl('', [Validators.required]),
+        dateTo: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: [this.dateControl],
+      }
+    );
   }
   public hasError(controlName: string): boolean {
     const control = this.getControl(controlName);
@@ -57,29 +78,25 @@ export class NewTripForm implements OnInit {
     errorMessage += errors['minlength']
       ? `🔥 More than ${errors['minlength'].requiredLength} chars`
       : ' ';
-      errorMessage += errors['min'] ? `🔥 Field needs more ${errors['min'].min} ` : ' ';
-      errorMessage += errors['max'] ? `🔥 Field needs less ${errors['max'].max}` : ' ';
+    errorMessage += errors['min']
+      ? `🔥 Field needs more ${errors['min'].min} `
+      : ' ';
+    errorMessage += errors['max']
+      ? `🔥 Field needs less ${errors['max'].max}`
+      : ' ';
     return errorMessage;
   }
-  onSave() {
-    const { destination, agencyId, plazas, precio, dateFrom, dateTo } = this.form.value;
-    const id = this.getDashId(destination, agencyId );
-    const newTripData = { id,destination, agencyId, plazas, precio, dateFrom, dateTo};
-    console.warn('Send trip data ', newTripData);
 
-  }
-
-  dateControl(form: AbstractControl): ValidationErrors | null{
-
+  dateControl(form: AbstractControl): ValidationErrors | null {
     const dateForm = form.get('dateFrom');
     const dateTo = form.get('dateTo');
 
-    if(!dateForm || !dateTo) {
+    if (!dateForm || !dateTo) {
       return {
         dateControl: 'Dates dont exist',
       };
     }
-    if(dateForm.value> dateTo.value) {
+    if (dateForm.value > dateTo.value) {
       return {
         dateControl: 'Dates wrong',
       };
@@ -91,21 +108,39 @@ export class NewTripForm implements OnInit {
     return this.form.get(controlName);
   }
 
-  private getDashId(destino: string , id:string): string {
-    const str = destino+' '+id+' '+this.nextnumber();
+  private getDashId(destino: string, id: string): string {
+    const str = destino + ' ' + id + ' ' + this.nextnumber();
     return str.toLocaleLowerCase().replace(/ /g, '-');
-
   }
-    private nextnumber(){
-      return this.autoN++;
-    }
+  private nextnumber() {
+    return this.autoN++;
+  }
 
-    getDateMessage(){
-      const errors = this.form.errors;
-      if(!errors){return ''};
-      if(errors['dateControl']){return errors ['dateControl']};
+  getDateMessage() {
+    const errors = this.form.errors;
+    if (!errors) {
       return '';
     }
+    if (errors['dateControl']) {
+      return errors['dateControl'];
+    }
+    return '';
+  }
 
+  onSave() {
+    const { destination, agencyId, plazas, precio, dateFrom, dateTo } =
+      this.form.value;
+    const id = this.getDashId(destination, agencyId);
+    const newTripData = {
+      id,
+      destination,
+      agencyId,
+      plazas,
+      precio,
+      dateFrom,
+      dateTo,
+    };
+    console.warn('Send trip data ', newTripData);
+  }
   ngOnInit(): void {}
 }
