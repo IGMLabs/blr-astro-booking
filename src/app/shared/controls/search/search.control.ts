2fe9954177351c6142a306c9abfda3ca9e03fc59
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-06-14 13:30:34 +0200

    search control

---
+++ src/app/shared/controls/search/search.control.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { fromEvent, Observable, tap } from 'rxjs';
+import { debounce, debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.control.html',
+  styleUrls: ['./search.control.css']
+})
+export class SearchControl implements OnInit {
+
+  @ViewChild('searchInput', {static: true}) searchInput!: ElementRef;
+
+  @Output() search = new EventEmitter<string>();
+
+  searchInput$!: Observable<string>;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.searchInput$=fromEvent(this.searchInput.nativeElement, 'keyup')
+    .pipe(
+      map((event: unknown) => {
+        return (event as any).target.value
+      }),
+      tap((searchTerm)=> console.log('antes',searchTerm)),
+      debounceTime(1000),
+      tap((searchTerm)=> console.log('despues',searchTerm)),
+      filter((search)=>search.length > 2 ),
+      tap((searchTerm)=> console.log('filtrado',searchTerm)),
+      distinctUntilChanged(),
+      tap((searchTerm)=> console.log('cambiado',searchTerm)),
+      tap((searchtermino)=> this.search.emit(searchtermino))
+    );
+  }
+
+}
